b3263849183bc9d4579b2521ba4f38ceae2baeef
hui <hui8958@gmail.com>
2016-05-05 15:45:16 +0200

    add random selection on timer

---
+++ app/src/test/java/chalmers/eda397_2016_group3/timer/MyTimerTest.java
@@ -0,0 +1,46 @@
+package chalmers.eda397_2016_group3.timer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hui on 28/04/16.
+ */
+public class MyTimerTest {
+
+    @Test
+    public void testSetCallback() throws Exception {
+
+    }
+
+    @Test
+    public void testIsRunning() throws Exception {
+
+    }
+
+    @Test
+    public void testIsPaused() throws Exception {
+
+    }
+
+    @Test
+    public void testStart() throws Exception {
+
+    }
+
+    @Test
+    public void testStop() throws Exception {
+
+    }
+
+    @Test
+    public void testPause() throws Exception {
+
+    }
+
+    @Test
+    public void testResume() throws Exception {
+
+    }
+}
---
+++ app/src/test/java/chalmers/eda397_2016_group3/utils/PairsTest.java
@@ -0,0 +1,16 @@
+package chalmers.eda397_2016_group3.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hui on 03/05/16.
+ */
+public class PairsTest {
+
+    @Test
+    public void testGetRandomPair() throws Exception {
+
+    }
+}
